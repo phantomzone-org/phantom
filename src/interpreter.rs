0586d8595a3845434f39c79f67ddcea168691ac0
Jean-Philippe Bossuat <jeanphilippe.bossuat@gmail.com>
2025-02-06 16:32:14 +0100

    started working on the interpreter

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,11 +1,14 @@
 //use crate::gadget::Gadget;
-//use crate::circuit_bootstrapping::CircuitBootstrapper;
 use crate::address::Address;
+use crate::circuit_bootstrapping::CircuitBootstrapper;
 use crate::memory::Memory;
-use base2k::VecZnx;
+use crate::parameters::{LOGN_DECOMP, VMPPMAT_COLS, VMPPMAT_ROWS};
+use base2k::{Module, VecZnx, VecZnxOps};
 
 pub struct Interpreter {
-    pub log_base_rgsw: usize,
+    pub log_k: usize,
+    pub log_base2k: usize,
+    pub max_address: usize,
     pub counter: Address,
     pub rs1_addresses: Memory,
     pub rs2_addresses: Memory,
@@ -14,76 +17,130 @@ pub struct Interpreter {
     pub op_it_memory: Memory,
     pub op_id_counter: Memory,
     pub rd: Memory,
-    pub register: VecZnx,
+    pub register: Memory,
     pub memory: Memory,
     pub ret: bool,
 }
 
 impl Interpreter {
-    /*
-    pub fn new(ring: &Ring<u64>, log_base_rgsw: usize, max_counter: usize) -> Self {
+    pub fn new(
+        module: &Module,
+        log_base2k: usize,
+        limbs: usize,
+        max_counter: usize,
+        max_address: usize,
+    ) -> Self {
+        let log_n: usize = module.log_n();
+        let log_k: usize = log_base2k * limbs;
         Self {
-            log_base_rgsw: log_base_rgsw,
-            counter: Address::new(ring, log_base_rgsw, ring.log_n(), max_counter),
-            rs1_addresses: Memory::new(ring),
-            rs2_addresses: Memory::new(ring),
-            imm: Memory::new(ring),
-            op_id_register: Memory::new(ring),
-            op_it_memory: Memory::new(ring),
-            op_id_counter: Memory::new(ring),
-            rd: Memory::new(ring),
-            register: ring.new_poly(),
-            memory: Memory::new(ring),
+            log_k: log_k,
+            log_base2k: log_base2k,
+            max_address: max_address,
+            counter: Address::new(module, LOGN_DECOMP, max_counter, VMPPMAT_ROWS, limbs + 1),
+            rs1_addresses: Memory::new(log_n, log_base2k, log_k),
+            rs2_addresses: Memory::new(log_n, log_base2k, log_k),
+            imm: Memory::new(log_n, log_base2k, log_k),
+            op_id_register: Memory::new(log_n, log_base2k, log_k),
+            op_it_memory: Memory::new(log_n, log_base2k, log_k),
+            op_id_counter: Memory::new(log_n, log_base2k, log_k),
+            rd: Memory::new(log_n, log_base2k, log_k),
+            register: Memory::new(log_n, log_base2k, log_k),
+            memory: Memory::new(log_n, log_base2k, log_k),
             ret: false,
         }
     }
 
-    pub fn init(&mut self, ring: &Ring<u64>) {
-        self.counter.set(ring, 0);
+    pub fn init(&mut self, module: &Module) {
+        self.counter.set(module, 0);
     }
 
-    pub fn next(&mut self, ring_circuit_bootstrapping: &Ring<u64>, ring: &Ring<u64>) {
-        let rs1_address: u64 = self.rs1_addresses.read(ring, &self.counter);
-        let rs2_address: u64 = self.rs2_addresses.read(ring, &self.counter);
-        let imm: u64 = self.imm.read(ring, &self.counter);
-        let op_id_register: u64 = self.op_id_register.read(ring, &self.counter);
-        let op_it_memory: u64 = self.op_it_memory.read(ring, &self.counter);
-        let op_id_counter: u64 = self.op_id_counter.read(ring, &self.counter);
+    pub fn next(&mut self, module_pbs: &Module, module_lwe: &Module) {
+        let log_n_decomp: usize = LOGN_DECOMP;
+        let rows: usize = VMPPMAT_ROWS;
+        let cols: usize = VMPPMAT_COLS;
+
+        let rs1_idx_lwe: i64 = self.rs1_addresses.read(module_lwe, &self.counter);
+        let rs2_idx_lwe: i64 = self.rs2_addresses.read(module_lwe, &self.counter);
+        let imm_lwe: i64 = self.imm.read(module_lwe, &self.counter);
+        let op_id_register_lwe: i64 = self.op_id_register.read(module_lwe, &self.counter);
+        let op_it_memory_lwe: i64 = self.op_it_memory.read(module_lwe, &self.counter);
+        let op_id_counter_lwe: i64 = self.op_id_counter.read(module_lwe, &self.counter);
 
         let log_gap: usize = 6;
-        let log_base_rgsw = self.log_base_rgsw;
+        let log_base2k = self.log_base2k;
 
-        //let circuit_bootstrapper: CircuitBootstrapper = CircuitBootstrapper::new();
+        let circuit_bootstrapper: CircuitBootstrapper =
+            CircuitBootstrapper::new(&module_pbs, module_lwe.log_n(), log_base2k, cols);
 
-        //circuit_bootstrapper.init(ring_circuit_bootstrapping, log_gap, log_base_rgsw);
+        let mut buf_pbs: VecZnx = module_pbs.new_vec_znx(cols);
 
-        let mut buf_acc_0: Poly<u64> = ring_circuit_bootstrapping.new_poly();
-        let mut buf_acc_1: Poly<u64> = ring_circuit_bootstrapping.new_poly();
-        let mut buf_acc_2: Poly<u64> = ring_circuit_bootstrapping.new_poly();
+        let mut rs1_address: Address =
+            Address::new(&module_lwe, log_n_decomp, self.max_address, rows, cols);
+        circuit_bootstrapper.bootstrap_to_address(
+            module_pbs,
+            module_lwe,
+            rs1_idx_lwe as u32,
+            &mut rs1_address,
+            &mut buf_pbs,
+        );
 
-        /*
-        let mut rs1_address_gadget: Gadget<Poly<u64>> = Gadget::new(&ring, log_base);
-        circuit_bootstrapper.circuit_bootstrap(ring_circuit_bootstrapping, rs1_address as usize, &mut buf_acc_0, &mut buf_acc_1, &mut buf_acc_2, &mut rs1_address_gadget);
+        let mut rs2_address: Address =
+            Address::new(&module_lwe, log_n_decomp, self.max_address, rows, cols);
+        circuit_bootstrapper.bootstrap_to_address(
+            module_pbs,
+            module_lwe,
+            rs2_idx_lwe as u32,
+            &mut rs2_address,
+            &mut &mut buf_pbs,
+        );
 
-        let mut rs2_address_gadget: Gadget<Poly<u64>> = Gadget::new(&ring, log_base);
-        circuit_bootstrapper.circuit_bootstrap(ring_circuit_bootstrapping, rs2_address as usize, &mut buf_acc_0, &mut buf_acc_1, &mut buf_acc_2, &mut rs2_address_gadget);
+        let mut imm_address: Address =
+            Address::new(&module_lwe, log_n_decomp, self.max_address, rows, cols);
+        circuit_bootstrapper.bootstrap_to_address(
+            module_pbs,
+            module_lwe,
+            imm_lwe as u32,
+            &mut imm_address,
+            &mut &mut buf_pbs,
+        );
 
-        let mut imm_gadget: Gadget<Poly<u64>> = Gadget::new(&ring, log_base);
-        circuit_bootstrapper.circuit_bootstrap(ring_circuit_bootstrapping, imm as usize, &mut buf_acc_0, &mut buf_acc_1, &mut buf_acc_2, &mut imm_gadget);
+        let mut op_id_register_address: Address =
+            Address::new(&module_lwe, log_n_decomp, self.max_address, rows, cols);
+        circuit_bootstrapper.bootstrap_to_address(
+            module_pbs,
+            module_lwe,
+            op_id_register_lwe as u32,
+            &mut op_id_register_address,
+            &mut &mut buf_pbs,
+        );
 
-        let mut op_id_register_gadget: Gadget<Poly<u64>> = Gadget::new(&ring, log_base);
-        circuit_bootstrapper.circuit_bootstrap(ring_circuit_bootstrapping, op_id_register as usize, &mut buf_acc_0, &mut buf_acc_1, &mut buf_acc_2, &mut op_id_register_gadget);
+        let mut op_it_memory_address: Address =
+            Address::new(&module_lwe, log_n_decomp, self.max_address, rows, cols);
+        circuit_bootstrapper.bootstrap_to_address(
+            module_pbs,
+            module_lwe,
+            op_it_memory_lwe as u32,
+            &mut op_it_memory_address,
+            &mut &mut buf_pbs,
+        );
 
-        let mut op_it_memory_gadget: Gadget<Poly<u64>> = Gadget::new(&ring, log_base);
-        circuit_bootstrapper.circuit_bootstrap(ring_circuit_bootstrapping, op_it_memory as usize, &mut buf_acc_0, &mut buf_acc_1, &mut buf_acc_2, &mut op_it_memory_gadget);
+        let mut op_id_counter_address: Address =
+            Address::new(&module_lwe, log_n_decomp, self.max_address, rows, cols);
+        circuit_bootstrapper.bootstrap_to_address(
+            module_pbs,
+            module_lwe,
+            op_id_counter_lwe as u32,
+            &mut op_id_counter_address,
+            &mut &mut buf_pbs,
+        );
 
-        let mut op_id_counter_gadget: Gadget<Poly<u64>> = Gadget::new(&ring, log_base);
-        circuit_bootstrapper.circuit_bootstrap(ring_circuit_bootstrapping, op_id_counter as usize, &mut buf_acc_0, &mut buf_acc_1, &mut buf_acc_2, &mut op_id_counter_gadget);
-         */
         // Retrieves rs1 and rs2
-        //let rs1 = self.register.read(ring, Address(Vec::from_raw(&rs1_address_gadget)));
+        let rs1_lwe = self.register.read(module_lwe, &rs1_address);
+
+        let rs2_lwe = self.register.read(module_lwe, &rs2_address);
+
+        let imm_lwe = self.register.read(module_lwe, &imm_address);
 
         // Evaluates all OPS
     }
-    */
 }
--- src/parameters.rs
+++ src/parameters.rs
@@ -1,2 +1,5 @@
-pub const LOGNPBS: usize = 12;
-pub const LOGNLWE: usize = 11;
+pub const LOGN_PBS: usize = 12;
+pub const LOGN_LWE: usize = 11;
+pub const LOGN_DECOMP: usize = 6;
+pub const VMPPMAT_ROWS: usize = 4;
+pub const VMPPMAT_COLS: usize = 5;
