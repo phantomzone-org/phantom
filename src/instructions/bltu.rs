a71671cadecb10d8601905a30b0ca9fbc0120b24
Jean-Philippe Bossuat <jeanphilippe.bossuat@gmail.com>
2025-02-07 16:45:18 +0100

    typos

--- src/instructions/bltu.rs
+++ src/instructions/bltu.rs
@@ -1,10 +1,10 @@
 //! | imm[19:16] | imm[15:12] | imm[11:8] | imm[7:4] | imm[3:0] | rs2 | rs1 | rd | if (x[rs1] <u  x[rs2]), pc += sext(imm[19:0])
 
-use super::{Counter, sext};
+use super::{Arithmetic, sext};
 
 pub struct Bltu();
 
-impl Counter for Blut{
+impl Arithmetic for Blut{
     fn apply(&self, imm_19: u32, imm_15: u32, imm_11: u32, imm_7: u32, imm_3: u32, x_rs1: u32, x_rs2: u32, pc: u32) -> (u32, u32){
         if x_rs1 < x_rs2{
             (0, sext(imm_19<<16 | imm_11<<12 | imm_11<<8 | imm_7<<4 | imm_3, 12))
